42432369c451d5205065ca23e3c750b71a36dbe7
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2020-04-23 00:42:18 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+
+
+def main():
+    # Create the linear solver with the GLOP backend.
+    solver = pywraplp.Solver('simple_lp_program',
+                             pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
+    x = solver.NumVar(0, 1, 'x')
+    y = solver.NumVar(0, 2, 'y')
+
+    print('Number of variables =', solver.NumVariables())
+
+
+if __name__ == '__main__':
+    main()
